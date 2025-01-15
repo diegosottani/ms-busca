210e568da68d9464e7c344fef4460a645c336ce1
Diego <diegosottani@gmail.com>
2025-01-14 22:25:28 -0300

    Inserindo título e mock de resultados

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,39 @@ import { createClient } from "@supabase/supabase-js";
 //const supabase = createClient("https://<project>.supabase.co", "<your-anon-key>");
 
 function App() {
-  const [results, setResults] = useState([
-    "Resultado 1: Informações detalhadas",
-    "Resultado 2: Mais detalhes aqui",
-    "Resultado 3: Outra informação relevante",
-  ]);
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getList();
   }, []);
 
-  async function getList() {
-    //const { data } = await supabase.from("countries").select();
-    //setCountries(data);
-  }
+  async function getList() {}
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      setResults([
+        "Resultado 1: Informações detalhadas",
+        "Resultado 2: Mais detalhes aqui",
+        "Resultado 3: Outra informação relevante",
+      ]);
+
+      //const { data } = await supabase.from("countries").select();
+      //setResults(data);
+    } catch (error) {
+      alert("Erro ao tentar buscar informações!", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="flex justify-center items-center flex-col h-screen bg-gradient-to-br from-teal-500 to-green-700">
-      <form className="bg-white shadow-lg rounded-lg p-6 w-full max-w-md">
+      <h1 className="text-6xl font-bold text-white mb-8">MS Busca</h1>
+
+      <form className="bg-white shadow-lg rounded-lg p-6 w-full max-w-md" onSubmit={handleSearch}>
         <div className="relative">
           <input
             type="text"
@@ -36,6 +51,12 @@ function App() {
           </button>
         </div>
       </form>
+
+      {loading && (
+        <div className="flex justify-center items-center h-screen">
+          <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-slate-200 border-opacity-75"></div>
+        </div>
+      )}
 
       {results.length > 0 && (
         <div className="mt-6 bg-white shadow-md rounded-lg p-4 w-full max-w-md">
